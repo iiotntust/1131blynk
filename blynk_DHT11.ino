12e92794edb326ca9bcf59bdd2da719acd66fe99
iiotntust <56021651+iiotntust@users.noreply.github.com>
2024-10-06 22:15:08 +0800

    Add files via upload

---
+++ blynk_DHT11.ino
@@ -0,0 +1,59 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL6bVwW9iX4"
+#define BLYNK_TEMPLATE_NAME "test"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SoftwareSerial.h> // Arduino IDE <1.6.6
+#include <BlynkSimpleEsp8266.h>
+
+#include <DHT.h> 
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+//#define BLYNK_AUTH_TOKEN "yJF5LkUoaLFb5jMEkic_yktkXMYZ13a1"
+char auth[] = "yJF5LkUoaLFb5jMEkic_yktkXMYZ13a1";
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "AD-LLAN";
+char pass[] = "louis9617";
+ 
+#define DHTPIN D4          // What digital pin we're connected to
+#define DHTTYPE DHT11     // DHT 11<p>DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+float t;
+float h;
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  timer.setInterval(1000L, sendSensor);
+}
+ 
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
+ 
+ 
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendSensor()
+{
+  h = dht.readHumidity();
+  t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+//  l = analogRead(LDR);
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+}
